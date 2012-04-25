19357a687c35301b8998354ecdb6ae4b440eeaea
mehdi.assem@gmail.com <mehdi.assem@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-04-25 12:34:33 +0000

    [maven-release-plugin]  copy for tag 1.0.0

---
+++ mail-hibernate/tags/1.0.0/src/main/java/com/google/code/jee/utils/mail/hibernate/dao/MailAttachmentDao.java
@@ -0,0 +1,70 @@
+package com.google.code.jee.utils.mail.hibernate.dao;
+
+import java.util.List;
+
+import com.google.code.jee.utils.dal.dao.GenericDao;
+import com.google.code.jee.utils.mail.hibernate.model.MailAttachment;
+
+/**
+ * The Interface MailAttachmentDao.
+ */
+public interface MailAttachmentDao extends GenericDao<Integer, MailAttachment> {
+    String COUNT_BY_NAME = "mailAttachment.countByName";
+    String FIND_BY_NAME = "mailAttachment.findByName";
+    String COUNT_FOR_MAIL_ID = "mailAttachment.countForMailId";
+    String FIND_ALL_BY_MAIL_ID = "mailAttachment.findAllByMailId";
+    String COUNT_FOR_MAIL_ID_AND_NAME = "mailAttachment.countForMailIdAndName";
+    String FIND_BY_MAIL_ID_AND_NAME = "mailAttachment.findByMailIdAndName";
+
+    /**
+     * Search the number of elements with the 'name' parameter.
+     * 
+     * @param name the name
+     * @return the number of element found
+     */
+    Integer countByName(String name);
+
+    /**
+     * Search an element by its name.
+     * 
+     * @param name the name
+     * @return the mail
+     */
+    MailAttachment findByName(String name);
+
+    /**
+     * Count the number of attachments of a specific mail
+     * 
+     * @param mailId the mail id
+     * @return the number of attachments
+     */
+    Integer countForMailId(Integer mailId);
+
+    /**
+     * Finds the all mail attachments.
+     * 
+     * @param mailId the mail primary key
+     * @return the list
+     */
+    List<MailAttachment> findAllByMailId(Integer mailId);
+
+    /**
+     * Count the number of attachment with a specific name and corresponding to
+     * a specific mail
+     * 
+     * @param mailId the mailId
+     * @param attachmentName the attachment name
+     * @return the number of attachments
+     */
+    Integer countForMailIdAndName(Integer mailId, String attachmentName);
+
+    /**
+     * Finds the mail attachment.
+     * 
+     * @param mailId the mail primary key
+     * @param attachmentName the attachment name
+     * @return the mail attachment
+     */
+    MailAttachment findByMailIdAndName(Integer mailId, String attachmentName);
+
+}
---
+++ mail-hibernate/tags/1.0.0/src/main/java/com/google/code/jee/utils/mail/hibernate/dao/impl/MailDaoImpl.java
@@ -0,0 +1,143 @@
+package com.google.code.jee.utils.mail.hibernate.dao.impl;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.google.code.jee.utils.dal.Search;
+import com.google.code.jee.utils.dal.SearchCriteria;
+import com.google.code.jee.utils.dal.SortOrder;
+import com.google.code.jee.utils.dal.dao.AbstractGenericDaoHibernate;
+import com.google.code.jee.utils.mail.hibernate.dao.MailDao;
+import com.google.code.jee.utils.mail.hibernate.model.Mail;
+
+/**
+ * The Class MailDaoImpl.
+ */
+@Repository
+public class MailDaoImpl extends AbstractGenericDaoHibernate<Integer, Mail> implements MailDao {
+
+    /**
+     * Instantiates a new mail dao impl.
+     */
+    public MailDaoImpl() {
+        super(Mail.class);
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Integer countByName(String name) {
+        return this.getNumberByNamedQueryAndNamedParam(MailDao.COUNT_BY_NAME, new String[] { "name" }, name);
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Mail findByName(String name) {
+        return this.getByNamedQueryAndNamedParam(MailDao.FIND_BY_NAME, new String[] { "name" }, name);
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    protected Search getSearch(SearchCriteria searchCriteria) {
+        Search search = null;
+        if (searchCriteria != null) {
+            search = new Search();
+
+            final StringBuilder buffer = new StringBuilder();
+            buffer.append("from Mail m ");
+            if (searchCriteria.hasFilters()) {
+                buffer.append("where ");
+                int index = 0;
+                for (final Map.Entry<String, Object> entry : searchCriteria.getFilters().entrySet()) {
+                    if (entry.getValue() != null) {
+                        if (index != 0) {
+                            buffer.append("AND ");
+                        }
+                        if (entry.getKey().equals("name")) {
+                            buffer.append("upper(m.name) like upper(:name) ");
+                            search.addStringParameter("name", entry.getValue());
+                        } else if (entry.getKey().equals("from")) {
+                            buffer.append("upper(m.from) like upper(:from) ");
+                            search.addStringParameter("from", entry.getValue());
+                        } else if (entry.getKey().equals("replyTo")) {
+                            buffer.append("upper(m.replyTo) like upper(:replyTo) ");
+                            search.addStringParameter("replyTo", entry.getValue());
+                        } else if (entry.getKey().equals("subject")) {
+                            buffer.append("upper(m.subject) like upper(:subject) ");
+                            search.addStringParameter("subject", entry.getValue());
+                        } else if (entry.getKey().equals("text")) {
+                            buffer.append("upper(m.text) like upper(:text) ");
+                            search.addStringParameter("text", entry.getValue());
+                        } else if (entry.getKey().equals("htmlMessage")) {
+                            buffer.append("m.htmlMessage = :htmlMessage ");
+                            search.addBooleanParameter("htmlMessage", entry.getValue());
+                        } else if (entry.getKey().equals("template")) {
+                            buffer.append("m.template = :template ");
+                            search.addBooleanParameter("template", entry.getValue());
+                        }
+                        index++;
+                    }
+                }
+            }
+
+            search.setCountQuery("select count(*) " + buffer.toString());
+
+            if (searchCriteria.hasSorts()) {
+                buffer.append("order by ");
+                int index = 0;
+                for (final Map.Entry<String, SortOrder> entry : searchCriteria.getSorts().entrySet()) {
+                    if (index != 0) {
+                        buffer.append(", ");
+                    }
+                    if (entry.getKey().equals("name")) {
+                        buffer.append("m.name ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    } else if (entry.getKey().equals("from")) {
+                        buffer.append("m.from ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    } else if (entry.getKey().equals("replyTo")) {
+                        buffer.append("m.replyTo ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    } else if (entry.getKey().equals("subject")) {
+                        buffer.append("m.subject ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    } else if (entry.getKey().equals("text")) {
+                        buffer.append("m.text ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    } else if (entry.getKey().equals("htmlMessage")) {
+                        buffer.append("m.htmlMessage ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    } else if (entry.getKey().equals("template")) {
+                        buffer.append("m.template ");
+                        if (entry.getValue() == SortOrder.DESCENDING) {
+                            buffer.append("desc ");
+                        }
+                    }
+
+                    index++;
+                }
+            }
+
+            search.setQuery(buffer.toString());
+        }
+        return search;
+    }
+}

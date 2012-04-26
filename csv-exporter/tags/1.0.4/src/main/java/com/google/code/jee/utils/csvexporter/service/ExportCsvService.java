ad8dc38fd73ace7ef8c24eab57e364a57e674d8b
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-04-26 11:56:39 +0000

    [maven-release-plugin]  copy for tag 1.0.4

---
+++ csv-exporter/tags/1.0.4/src/main/java/com/google/code/jee/utils/csvexporter/service/ExportCsvService.java
@@ -0,0 +1,30 @@
+package com.google.code.jee.utils.csvexporter.service;
+
+import java.io.OutputStream;
+import java.util.List;
+
+import com.google.code.jee.utils.csvexporter.exception.ExportCsvException;
+
+/**
+ * The Interface ExportCsvService.
+ */
+public interface ExportCsvService {
+
+    /**
+     * Export.
+     * 
+     * @param elements the elements
+     * @return the list< string>
+     * @throws ExportCsvException the export csv exception
+     */
+    List<String> export(List<?> elements) throws ExportCsvException;
+
+    /**
+     * Export.
+     * 
+     * @param outputStream the output stream
+     * @param elements the elements
+     * @throws ExportCsvException the export csv exception
+     */
+    void export(OutputStream outputStream, List<?> elements) throws ExportCsvException;
+}

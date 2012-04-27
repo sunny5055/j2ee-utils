4242e29f1a054aeae3cfad0b119445969881546d
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-04-27 12:47:01 +0000

    [maven-release-plugin]  copy for tag 1.0.0

---
+++ jsf-archetype/tags/1.0.0/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/web/util/AppUtil.java
@@ -0,0 +1,19 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.web.util;
+
+
+/**
+ * The class AppUtil.
+ */
+public final class AppUtil {
+
+    /**
+     * Instantiates a new app util.
+     */
+    private AppUtil() {
+        super();
+    }
+
+}
---
+++ jsf-archetype/tags/1.0.0/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/webapp/index.jsp
@@ -0,0 +1,8 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+<html>
+<body>
+<jsp:forward page="/faces/xhtml/index.xhtml"/>
+</body>
+</html>

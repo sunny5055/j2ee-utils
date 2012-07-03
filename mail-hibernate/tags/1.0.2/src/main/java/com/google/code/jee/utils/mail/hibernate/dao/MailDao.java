e63b1cab97aa21e3e2417960291d7a069a82b269
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-07-03 08:34:46 +0000

    [maven-release-plugin]  copy for tag 1.0.2

---
+++ mail-hibernate/tags/1.0.2/src/main/java/com/google/code/jee/utils/mail/hibernate/dao/MailDao.java
@@ -0,0 +1,28 @@
+package com.google.code.jee.utils.mail.hibernate.dao;
+
+import com.google.code.jee.utils.dal.dao.GenericDao;
+import com.google.code.jee.utils.mail.hibernate.model.Mail;
+
+/**
+ * The Interface MailDao.
+ */
+public interface MailDao extends GenericDao<Integer, Mail> {
+    String COUNT_BY_NAME = "mail.countByName";
+    String FIND_BY_NAME = "mail.findByName";
+
+    /**
+     * Search the number of elements with the 'name' parameter.
+     * 
+     * @param name the name
+     * @return the number of elements found
+     */
+    Integer countByName(String name);
+
+    /**
+     * Search an element by its name.
+     * 
+     * @param name the name
+     * @return the mail
+     */
+    Mail findByName(String name);
+}

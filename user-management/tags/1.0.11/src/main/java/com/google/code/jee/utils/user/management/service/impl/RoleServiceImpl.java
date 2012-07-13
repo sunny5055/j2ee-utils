c2dd64484366f947d4b2fdcbed92d34fda1cffea
neuhart.julien@gmail.com <neuhart.julien@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-07-13 08:20:58 +0000

    [maven-release-plugin]  copy for tag 1.0.11

---
+++ user-management/tags/1.0.11/src/main/java/com/google/code/jee/utils/user/management/service/impl/RoleServiceImpl.java
@@ -0,0 +1,95 @@
+package com.google.code.jee.utils.user.management.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.code.jee.utils.StringUtil;
+import com.google.code.jee.utils.dal.service.AbstractGenericService;
+import com.google.code.jee.utils.user.management.dao.RoleDao;
+import com.google.code.jee.utils.user.management.model.Role;
+import com.google.code.jee.utils.user.management.service.RoleService;
+import com.google.code.jee.utils.user.management.service.UserService;
+
+/**
+ * The Class RoleServiceImpl.
+ */
+@Service
+public class RoleServiceImpl extends AbstractGenericService<Integer, Role, RoleDao> implements RoleService {
+	
+	@Autowired
+	private UserService userService;
+	
+	/**
+     * {@inheritedDoc}
+     */
+    @Autowired
+    @Override
+    public void setDao(RoleDao dao) {
+        this.dao = dao;
+    }
+    
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Boolean existWithCode(String code) {
+    	Boolean exist = false;
+        if (!StringUtil.isEmpty(code)) {
+            final Integer count = this.dao.countByCode(code);
+            exist = count != 0;
+        }
+        return exist;
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Role findByCode(String code) {
+    	Role role = null;
+        if (!StringUtil.isEmpty(code))
+            role = this.dao.findByCode(code);
+        return role;
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Integer countByUserId(Integer userId) {
+    	return this.dao.countByUserId(userId);
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public List<Role> findAllByUserId(Integer userId) {
+    	List<Role> roles = null;
+        if (userId != null)
+            roles = this.dao.findAllByUserId(userId);
+        return roles;
+    }
+    
+    /**
+     * {@inheritedDoc}
+     */   
+    @Override
+    public boolean isRemovable(Integer roleId) {
+        if (this.userService.countByRoleId(roleId) != 0)
+        	return false;
+        else
+        	return true;
+    }
+
+    /**
+     * {@inheritedDoc}
+     */   
+	@Override
+	public Integer countByRightId(Integer rightId) {
+		return this.dao.countByRightId(rightId);
+	}
+
+}

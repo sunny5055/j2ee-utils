8fdfd399e7da435633fd56d405502c834efc127c
neuhart.julien@gmail.com <neuhart.julien@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-07-16 08:26:10 +0000

    [maven-release-plugin]  copy for tag 1.0.13

---
+++ user-management/tags/1.0.13/src/main/java/com/google/code/jee/utils/user/management/service/impl/RightServiceImpl.java
@@ -0,0 +1,87 @@
+package com.google.code.jee.utils.user.management.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.google.code.jee.utils.StringUtil;
+import com.google.code.jee.utils.dal.service.AbstractGenericService;
+import com.google.code.jee.utils.user.management.dao.RightDao;
+import com.google.code.jee.utils.user.management.model.Right;
+import com.google.code.jee.utils.user.management.service.RightService;
+import com.google.code.jee.utils.user.management.service.RoleService;
+
+/**
+ * The Class RightServiceImpl.
+ */
+@Service
+public class RightServiceImpl extends AbstractGenericService<Integer, Right, RightDao> implements RightService {
+
+	@Autowired
+	private RoleService roleService;
+	
+	/**
+     * {@inheritedDoc}
+     */
+    @Autowired
+    @Override
+    public void setDao(RightDao rightDao) {
+        this.dao = rightDao;
+    }
+    
+    /**
+     * {@inheritedDoc}
+     */   
+    @Override
+    public boolean isRemovable(Integer pk) {
+        if (this.roleService.countByRightId(pk) != 0)
+        	return false;
+        else
+        	return true;
+    }
+    
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Boolean existWithCode(String code) {
+    	Boolean exist = false;
+        if (!StringUtil.isEmpty(code)) {
+            final Integer count = this.dao.countByCode(code);
+            exist = count != 0;
+        }
+        return exist;
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Right findByCode(String code) {
+    	Right right = null;
+        if (!StringUtil.isEmpty(code))
+            right = this.dao.findByCode(code);
+        return right;
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public Integer countByRoleId(Integer roleId) {
+    	return this.dao.countByRoleId(roleId);
+    }
+
+    /**
+     * {@inheritedDoc}
+     */
+    @Override
+    public List<Right> findAllByRoleId(Integer roleId) {
+    	List<Right> rights = null;
+        if (roleId != null)
+            rights = this.dao.findAllByRoleId(roleId);
+        return rights;
+    }
+	
+}

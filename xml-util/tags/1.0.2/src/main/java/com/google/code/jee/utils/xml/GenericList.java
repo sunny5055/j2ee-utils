73f4f32a28bb42e78953cdd0312d5cb9580735d6
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-08-22 14:16:49 +0000

    [maven-release-plugin]  copy for tag 1.0.2

---
+++ xml-util/tags/1.0.2/src/main/java/com/google/code/jee/utils/xml/GenericList.java
@@ -0,0 +1,15 @@
+package com.google.code.jee.utils.xml;
+
+import java.util.List;
+
+public class GenericList {
+    private List<?> elements;
+
+    public List<?> getElements() {
+        return elements;
+    }
+
+    public void setElements(List<?> elements) {
+        this.elements = elements;
+    } 
+}

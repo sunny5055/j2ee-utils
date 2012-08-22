fe5c47f2d6bda3efeacadac4f71d15f08aac830e
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-08-22 14:28:37 +0000

    [maven-release-plugin]  copy for tag 1.0.5

---
+++ mail-sender/tags/1.0.5/src/main/java/com/google/code/jee/utils/mail/exception/MailSenderServiceException.java
@@ -0,0 +1,44 @@
+package com.google.code.jee.utils.mail.exception;
+
+/**
+ * The Class MailServiceException.
+ */
+@SuppressWarnings("serial")
+public class MailSenderServiceException extends Exception {
+
+    /**
+     * Instantiates a new mail service exception.
+     */
+    public MailSenderServiceException() {
+        super();
+    }
+
+    /**
+     * Instantiates a new mail service exception.
+     * 
+     * @param errorMessage the error message
+     */
+    public MailSenderServiceException(String errorMessage) {
+        super(errorMessage);
+    }
+
+    /**
+     * Instantiates a new mail service exception.
+     * 
+     * @param message the message
+     * @param cause the cause
+     */
+    public MailSenderServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    /**
+     * Instantiates a new mail service exception.
+     * 
+     * @param cause the cause
+     */
+    public MailSenderServiceException(Throwable cause) {
+        super(cause);
+    }
+
+}

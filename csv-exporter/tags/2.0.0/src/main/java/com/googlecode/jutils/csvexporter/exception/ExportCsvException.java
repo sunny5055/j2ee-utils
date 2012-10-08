2b0bf610eba1b112b1b84b8115895ef2bb11b878
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-10-08 18:35:28 +0000

    [maven-release-plugin]  copy for tag 2.0.0

---
+++ csv-exporter/tags/2.0.0/src/main/java/com/googlecode/jutils/csvexporter/exception/ExportCsvException.java
@@ -0,0 +1,44 @@
+package com.googlecode.jutils.csvexporter.exception;
+
+/**
+ * The Class ExportCsvException.
+ */
+@SuppressWarnings ("serial")
+public class ExportCsvException extends Exception {
+
+    /**
+     * The Constructor.
+     */
+    public ExportCsvException() {
+        super();
+    }
+
+    /**
+     * The Constructor.
+     * 
+     * @param msg the msg
+     * @param t the t
+     */
+    public ExportCsvException(String msg, Throwable t) {
+        super(msg, t);
+    }
+
+    /**
+     * The Constructor.
+     * 
+     * @param msg the msg
+     */
+    public ExportCsvException(String msg) {
+        super(msg);
+    }
+
+    /**
+     * The Constructor.
+     * 
+     * @param t the t
+     */
+    public ExportCsvException(Throwable t) {
+        super(t);
+    }
+
+}

2cb4061090edc6cdca6db9587f76688d9e5989d2
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-10-08 18:57:31 +0000

    [maven-release-plugin]  copy for tag 1.0.1

---
+++ oxm/tags/1.0.1/src/main/java/com/googlecode/jutils/oxm/service/OxmService.java
@@ -0,0 +1,160 @@
+package com.googlecode.jutils.oxm.service;
+
+import java.io.File;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+import com.googlecode.jutils.oxm.exception.OxmServiceException;
+
+/**
+ * Interface OxmService.
+ */
+public interface OxmService {
+
+	/**
+	 * Marshall.
+	 * 
+	 * @param value
+	 *            the value
+	 * @return the string
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	String marshall(Object value) throws OxmServiceException;
+
+	/**
+	 * Marshall.
+	 * 
+	 * @param value
+	 *            the value
+	 * @param xmlFile
+	 *            the xml file
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	void marshall(Object value, File xmlFile) throws OxmServiceException;
+
+	/**
+	 * Marshall.
+	 * 
+	 * @param value
+	 *            the value
+	 * @param xmlFile
+	 *            the xml file
+	 * @param encoding
+	 *            the encoding
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	void marshall(Object value, File xmlFile, String encoding) throws OxmServiceException;
+
+	/**
+	 * Marshall.
+	 * 
+	 * @param value
+	 *            the value
+	 * @param outputStream
+	 *            the output stream
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	void marshall(Object value, OutputStream outputStream) throws OxmServiceException;
+
+	/**
+	 * Marshall.
+	 * 
+	 * @param value
+	 *            the value
+	 * @param outputStream
+	 *            the output stream
+	 * @param encoding
+	 *            the encoding
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	void marshall(Object value, OutputStream outputStream, String encoding) throws OxmServiceException;
+
+	/**
+	 * Marshall.
+	 * 
+	 * @param value
+	 *            the value
+	 * @param writer
+	 *            the writer
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	void marshall(Object value, Writer writer) throws OxmServiceException;
+
+	/**
+	 * Unmarshall.
+	 * 
+	 * @param xmlContent
+	 *            the xml content
+	 * @return the object
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	Object unmarshall(String xmlContent) throws OxmServiceException;
+
+	/**
+	 * Unmarshall.
+	 * 
+	 * @param xmlFile
+	 *            the xml file
+	 * @return the object
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	Object unmarshall(File xmlFile) throws OxmServiceException;
+
+	/**
+	 * Unmarshall.
+	 * 
+	 * @param xmlFile
+	 *            the xml file
+	 * @param encoding
+	 *            the encoding
+	 * @return the object
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	Object unmarshall(File xmlFile, String encoding) throws OxmServiceException;
+
+	/**
+	 * Unmarshall.
+	 * 
+	 * @param inputStream
+	 *            the input stream
+	 * @return the object
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	Object unmarshall(InputStream inputStream) throws OxmServiceException;
+
+	/**
+	 * Unmarshall.
+	 * 
+	 * @param inputStream
+	 *            the input stream
+	 * @param encoding
+	 *            the encoding
+	 * @return the object
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	Object unmarshall(InputStream inputStream, String encoding) throws OxmServiceException;
+
+	/**
+	 * Unmarshall.
+	 * 
+	 * @param reader
+	 *            the reader
+	 * @return the object
+	 * @throws OxmServiceException
+	 *             the oxm service exception
+	 */
+	Object unmarshall(Reader reader) throws OxmServiceException;
+}

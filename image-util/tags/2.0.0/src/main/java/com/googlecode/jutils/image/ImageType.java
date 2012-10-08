2ca43de59a5cfd510274b211aac23848bd33bb65
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-10-08 19:09:08 +0000

    [maven-release-plugin]  copy for tag 2.0.0

---
+++ image-util/tags/2.0.0/src/main/java/com/googlecode/jutils/image/ImageType.java
@@ -0,0 +1,42 @@
+package com.googlecode.jutils.image;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.googlecode.jutils.StringUtil;
+
+/**
+ * The Enum ImageType.
+ * 
+ * @author James
+ */
+public enum ImageType {
+    JPG, GIF, PNG, UNKNOWN;
+
+    private static final Map<String, ImageType> extensionMap = new HashMap<String, ImageType>();
+
+    static {
+        extensionMap.put("jpg", ImageType.JPG);
+        extensionMap.put("jpeg", ImageType.JPG);
+        extensionMap.put("gif", ImageType.GIF);
+        extensionMap.put("png", ImageType.PNG);
+    }
+
+    /**
+     * Gets the type.
+     * 
+     * @param extension the extension
+     * @return the type
+     */
+    public static ImageType getType(String extension) {
+        ImageType imageType = UNKNOWN;
+        if (!StringUtil.isBlank(extension)) {
+            extension = extension.toLowerCase();
+            if (extensionMap.containsKey(extension)) {
+                imageType = extensionMap.get(extension);
+            }
+
+        }
+        return imageType;
+    }
+}

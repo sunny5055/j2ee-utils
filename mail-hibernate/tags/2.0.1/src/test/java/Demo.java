2656c3007d41f7b845af79a5abeca7610452ac6b
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-12-05 15:51:44 +0000

    [maven-release-plugin]  copy for tag 2.0.1

---
+++ mail-hibernate/tags/2.0.1/src/test/java/Demo.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.googlecode.jutils.mail.hibernate.model.Mail;
+import com.googlecode.jutils.mail.hibernate.service.MailService;
+
+public class Demo {
+    public static void main(String[] args) {
+        final ApplicationContext context = new ClassPathXmlApplicationContext("spring/application-context.xml");
+        final MailService mailService = context.getBean(MailService.class);
+
+        Mail mail = new Mail();
+
+        mail.setName("testMail");
+        mail.setFrom("malcolm.x@gmail.com");
+        mail.setReplyTo("malcolm.x@gmail.com");
+        mail.setSubject("Test");
+        mail.setText("This is a test !");
+        mail.setHtmlMessage(false);
+
+        mailService.create(mail);
+    }
+}

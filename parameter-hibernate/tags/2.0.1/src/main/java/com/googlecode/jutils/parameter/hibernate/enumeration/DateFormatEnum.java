a420d254e93825cf51d465b31a7a38897b85c42d
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2012-12-05 15:49:03 +0000

    [maven-release-plugin]  copy for tag 2.0.1

---
+++ parameter-hibernate/tags/2.0.1/src/main/java/com/googlecode/jutils/parameter/hibernate/enumeration/DateFormatEnum.java
@@ -0,0 +1,15 @@
+package com.googlecode.jutils.parameter.hibernate.enumeration;
+
+public enum DateFormatEnum {
+    DATE_TIME("dd/MM/yyyy HH:mm:ss"), DATE_ONLY("dd/MM/yyyy"), TIME_ONLY("HH:mm:ss");
+
+    private final String dateFormat;
+
+    private DateFormatEnum(String dateFormat) {
+        this.dateFormat = dateFormat;
+    }
+
+    public String getValue() {
+        return this.dateFormat;
+    }
+}

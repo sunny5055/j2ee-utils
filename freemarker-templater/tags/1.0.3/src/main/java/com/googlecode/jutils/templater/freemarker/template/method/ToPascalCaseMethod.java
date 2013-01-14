372a957f4922b2fc9d35da39255b3487ef62caa0
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-01-14 21:21:35 +0000

    [maven-release-plugin]  copy for tag 1.0.3

---
+++ freemarker-templater/tags/1.0.3/src/main/java/com/googlecode/jutils/templater/freemarker/template/method/ToPascalCaseMethod.java
@@ -0,0 +1,34 @@
+package com.googlecode.jutils.templater.freemarker.template.method;
+
+import java.util.List;
+
+import com.googlecode.jutils.StringUtil;
+import com.googlecode.jutils.collection.CollectionUtil;
+
+import freemarker.template.SimpleScalar;
+import freemarker.template.TemplateMethodModel;
+import freemarker.template.TemplateModel;
+import freemarker.template.TemplateModelException;
+
+/**
+ * The Class ToPascalCaseMethod.
+ */
+public class ToPascalCaseMethod implements TemplateMethodModel {
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	@SuppressWarnings("rawtypes")
+	public TemplateModel exec(List args) throws TemplateModelException {
+		SimpleScalar result = null;
+		if (!CollectionUtil.isEmpty(args)) {
+			final String value = MethodUtil.getRequiredParameter(args, 0, String.class);
+			if (!StringUtil.isBlank(value)) {
+				final String stringValue = StringUtil.toPascalCase(value);
+				result = new SimpleScalar(stringValue);
+			}
+		}
+		return result;
+	}
+}

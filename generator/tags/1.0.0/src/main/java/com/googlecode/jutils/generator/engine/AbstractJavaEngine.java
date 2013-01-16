145698602e6dd830a70c1b0f4fb03208f3c260f9
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-01-16 09:43:30 +0000

    [maven-release-plugin]  copy for tag 1.0.0

---
+++ generator/tags/1.0.0/src/main/java/com/googlecode/jutils/generator/engine/AbstractJavaEngine.java
@@ -0,0 +1,84 @@
+package com.googlecode.jutils.generator.engine;
+
+import java.util.Map;
+
+import com.googlecode.jutils.StringUtil;
+import com.googlecode.jutils.generator.formatter.impl.JavaFormatter;
+import com.googlecode.jutils.generator.freemarker.method.GetClassNameMethod;
+import com.googlecode.jutils.generator.freemarker.method.GetFqdnMethod;
+import com.googlecode.jutils.generator.freemarker.method.GetImportsMethod;
+import com.googlecode.jutils.generator.freemarker.method.GetModifiersMethod;
+import com.googlecode.jutils.generator.freemarker.method.GetPackageMethod;
+import com.googlecode.jutils.generator.freemarker.method.GetTypeMethod;
+import com.googlecode.jutils.generator.freemarker.method.IsPrimitiveMethod;
+
+public abstract class AbstractJavaEngine extends AbstractEngine {
+	protected static final String JAVA_PATH_KEY = "path.java";
+	protected static final String RESOURCES_PATH_KEY = "path.resources";
+	protected static final String WEBAPP_PATH_KEY = "path.webapp";
+	protected static final String CONFIG_PATH_KEY = "path.config";
+	protected static final String TEST_JAVA_PATH_KEY = "path.test_java";
+	protected static final String TEST_RESOURCES_PATH_KEY = "path.test_resources";
+
+	public AbstractJavaEngine() {
+		super();
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+
+		this.defaultProperties.put(JAVA_PATH_KEY, "src/main/java");
+		this.defaultProperties.put(RESOURCES_PATH_KEY, "src/main/resources");
+		this.defaultProperties.put(WEBAPP_PATH_KEY, "src/main/webapp");
+		this.defaultProperties.put(CONFIG_PATH_KEY, "src/main/config");
+		this.defaultProperties.put(TEST_JAVA_PATH_KEY, "src/test/java");
+		this.defaultProperties.put(TEST_RESOURCES_PATH_KEY, "src/test/resources");
+
+		this.defaultFormatters.add(new JavaFormatter());
+	}
+
+	@Override
+	protected void addFreemarkerExt(Map<String, Object> data) {
+		if (data != null) {
+			super.addFreemarkerExt(data);
+
+			data.put("getImports", new GetImportsMethod());
+			data.put("getPackage", new GetPackageMethod());
+			data.put("getClassName", new GetClassNameMethod());
+			data.put("getFqdn", new GetFqdnMethod());
+			data.put("getModifiers", new GetModifiersMethod());
+			data.put("getType", new GetTypeMethod());
+			data.put("isPrimitive", new IsPrimitiveMethod());
+		}
+	}
+
+	protected String getClassPathResource(String path) {
+		return getClassPathResource(path, null);
+	}
+
+	protected String getClassPathResource(String path, String fileName) {
+		String classPathResource = null;
+		if (!StringUtil.isBlank(path)) {
+			final String javaPath = resolveKey(JAVA_PATH_KEY);
+			final String resourcesPath = resolveKey(RESOURCES_PATH_KEY);
+			final String testJavaPath = resolveKey(TEST_JAVA_PATH_KEY);
+			final String testResourcesPath = resolveKey(TEST_RESOURCES_PATH_KEY);
+
+			classPathResource = path;
+			classPathResource = StringUtil.remove(classPathResource, javaPath);
+			classPathResource = StringUtil.remove(classPathResource, resourcesPath);
+			classPathResource = StringUtil.remove(classPathResource, testJavaPath);
+			classPathResource = StringUtil.remove(classPathResource, testResourcesPath);
+			classPathResource = StringUtil.removeStart(classPathResource, "/");
+			classPathResource = StringUtil.removeStart(classPathResource, "\\");
+			classPathResource = StringUtil.removeEnd(classPathResource, "/");
+			classPathResource = StringUtil.removeEnd(classPathResource, "\\");
+
+			if (!StringUtil.isBlank(fileName)) {
+				classPathResource += "/" + fileName;
+			}
+		}
+		return classPathResource;
+	}
+}

706caa3c9447984a70d304969a8848317d14022d
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-03-19 14:03:15 +0000

    Move to sandbox

--- sandbox/generator/tags/1.0.1/src/main/java/com/googlecode/jutils/generator/engine/Engine.java
+++
@@ -1,9 +0,0 @@
-package com.googlecode.jutils.generator.engine;
-
-import com.googlecode.jutils.generator.exception.GeneratorServiceException;
-
-public interface Engine {
-	String printHelp();
-
-	void generate(String xmlContent) throws GeneratorServiceException;
-}
--- sandbox/jsf-archetype/tags/1.0.0/trunk/src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-#set( $symbol_pound = '#' )
-#set( $symbol_dollar = '$' )
-#set( $symbol_escape = '\' )
-<html>
-<body>
-<jsp:forward page="/faces/xhtml/index.xhtml"/>
-</body>
-</html>

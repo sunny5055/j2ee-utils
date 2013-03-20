3844ccd11f7703ef1330cc02bf4fa6d6217e2e68
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-03-20 09:30:10 +0000

    [maven-release-plugin]  copy for tag 2.0.4

---
+++ main/i18n-hibernate/tags/2.0.4/src/main/java/com/googlecode/jutils/bundle/hibernate/dao/LabelDao.java
@@ -0,0 +1,49 @@
+package com.googlecode.jutils.bundle.hibernate.dao;
+
+import java.util.List;
+
+import com.googlecode.jutils.bundle.hibernate.model.Label;
+import com.googlecode.jutils.bundle.hibernate.model.LabelId;
+import com.googlecode.jutils.dal.dao.GenericDao;
+
+/**
+ * The Interface LabelDao.
+ */
+public interface LabelDao extends GenericDao<LabelId, Label> {
+    String COUNT_FOR_KEY = "label.countForKey";
+    String FIND_ALL_BY_KEY = "label.findAllByKey";
+    String COUNT_FOR_LANGUAGE = "label.countForLanguage";
+    String FIND_ALL_BY_LANGUAGE = "label.findAllByLanguage";
+
+    /**
+     * Search the number of elements with the 'key' parameter.
+     * 
+     * @param key the key
+     * @return the number of elements found
+     */
+    Integer countForKey(String key);
+
+    /**
+     * Search an element by its key.
+     * 
+     * @param key the key
+     * @return the mail
+     */
+    List<Label> findAllByKey(String key);
+
+    /**
+     * Count by language.
+     *
+     * @param language the language
+     * @return the number of elements found
+     */
+    Integer countForLanguage(String language);
+
+    /**
+     * Finds the labels by language.
+     *
+     * @param language the language
+     * @return the label
+     */
+    List<Label> findAllByLanguage(String language);
+}

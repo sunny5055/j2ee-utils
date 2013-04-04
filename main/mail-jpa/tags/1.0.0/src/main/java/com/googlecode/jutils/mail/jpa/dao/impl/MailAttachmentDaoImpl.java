f417aec016f0d4468b8ec60d764b1431d0a7815d
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-04-04 07:41:53 +0000

    [maven-release-plugin]  copy for tag 1.0.0

---
+++ main/mail-jpa/tags/1.0.0/src/main/java/com/googlecode/jutils/mail/jpa/dao/impl/MailAttachmentDaoImpl.java
@@ -0,0 +1,184 @@
+package com.googlecode.jutils.mail.jpa.dao.impl;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Repository;
+
+import com.googlecode.jutils.dal.Search;
+import com.googlecode.jutils.dal.SearchCriteria;
+import com.googlecode.jutils.dal.SortOrder;
+import com.googlecode.jutils.dal.dao.AbstractGenericJpaDao;
+import com.googlecode.jutils.dal.util.QueryUtil;
+import com.googlecode.jutils.mail.jpa.dao.MailAttachmentDao;
+import com.googlecode.jutils.mail.jpa.model.MailAttachment;
+
+/**
+ * The Class MailAttachmentDaoImpl.
+ */
+@Repository
+public class MailAttachmentDaoImpl extends AbstractGenericJpaDao<Integer, MailAttachment> implements MailAttachmentDao {
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	public Integer countByName(String name) {
+		return QueryUtil.getNumberByNamedQueryAndNamedParam(entityManager, MailAttachmentDao.COUNT_BY_NAME, new String[] { "name" }, name);
+	}
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	public MailAttachment findByName(String name) {
+		return QueryUtil.getByNamedQueryAndNamedParam(entityManager, MailAttachmentDao.FIND_BY_NAME, new String[] { "name" }, name);
+
+	}
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	public Integer countForMailId(Integer mailId) {
+		return QueryUtil.getNumberByNamedQueryAndNamedParam(entityManager, MailAttachmentDao.COUNT_FOR_MAIL_ID, new String[] { "mailId" }, mailId);
+	}
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	public List<MailAttachment> findAllByMailId(Integer mailId) {
+		return QueryUtil.findByNamedQueryAndNamedParam(entityManager, MailAttachmentDao.FIND_ALL_BY_MAIL_ID, new String[] { "mailId" }, mailId);
+	}
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	public Integer countForMailIdAndName(Integer mailId, String attachmentName) {
+		return QueryUtil.getNumberByNamedQueryAndNamedParam(entityManager, MailAttachmentDao.COUNT_FOR_MAIL_ID_AND_NAME, new String[] { "mailId", "attachmentName" }, mailId,
+				attachmentName);
+	}
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	public MailAttachment findByMailIdAndName(Integer mailId, String attachmentName) {
+		return QueryUtil.getByNamedQueryAndNamedParam(entityManager, MailAttachmentDao.FIND_BY_MAIL_ID_AND_NAME, new String[] { "mailId", "attachmentName" }, mailId,
+				attachmentName);
+	}
+
+	/**
+	 * Gets the search.
+	 * 
+	 * @param mailId
+	 *            the mail id
+	 * @param searchCriteria
+	 *            the search criteria
+	 * @return the search
+	 */
+	protected Search getSearch(Integer mailId, SearchCriteria searchCriteria) {
+		Search search = null;
+		if (mailId != null && searchCriteria != null) {
+			search = new Search();
+			final StringBuilder buffer = new StringBuilder();
+			buffer.append("from Mail m ");
+			buffer.append("left join mai.attachments as attachment ");
+			buffer.append("where m.id = :mailId ");
+			search.addIntegerParameter("mailId", mailId);
+
+			if (searchCriteria.hasFilters()) {
+				buffer.append("where ");
+				int index = 0;
+				for (final Map.Entry<String, Object> entry : searchCriteria.getFilters().entrySet()) {
+					if (entry.getValue() != null) {
+						if (index != 0) {
+							buffer.append("AND ");
+						}
+						if (entry.getKey().equals("name")) {
+							buffer.append("upper(attachment.name) like upper(:name) ");
+							search.addStringParameter("name", entry.getValue());
+						}
+						index++;
+					}
+				}
+			}
+
+			search.setCountQuery("select count(attachment) " + buffer.toString());
+
+			if (searchCriteria.hasSorts()) {
+				buffer.append("order by ");
+				int index = 0;
+				for (final Map.Entry<String, SortOrder> entry : searchCriteria.getSorts().entrySet()) {
+					if (index != 0) {
+						buffer.append(", ");
+					}
+					if (entry.getKey().equals("name")) {
+						buffer.append("attachment.name ");
+						if (entry.getValue() == SortOrder.DESCENDING) {
+							buffer.append("desc ");
+						}
+					}
+					index++;
+				}
+			}
+
+			search.setQuery("select attachment " + buffer.toString());
+		}
+		return search;
+	}
+
+	/**
+	 * {@inheritedDoc}
+	 */
+	@Override
+	protected Search getSearch(SearchCriteria searchCriteria) {
+		Search search = null;
+		if (searchCriteria != null) {
+			search = new Search();
+
+			final StringBuilder buffer = new StringBuilder();
+			buffer.append("from MailAttachment maa ");
+			if (searchCriteria.hasFilters()) {
+				buffer.append("where ");
+				int index = 0;
+				for (final Map.Entry<String, Object> entry : searchCriteria.getFilters().entrySet()) {
+					if (entry.getValue() != null) {
+						if (index != 0) {
+							buffer.append("AND ");
+						}
+						if (entry.getKey().equals("name")) {
+							buffer.append("upper(maa.name) like upper(:name) ");
+							search.addStringParameter("name", entry.getValue());
+						}
+						index++;
+					}
+				}
+			}
+
+			search.setCountQuery("select count(*) " + buffer.toString());
+
+			if (searchCriteria.hasSorts()) {
+				buffer.append("order by ");
+				int index = 0;
+				for (final Map.Entry<String, SortOrder> entry : searchCriteria.getSorts().entrySet()) {
+					if (index != 0) {
+						buffer.append(", ");
+					}
+					if (entry.getKey().equals("name")) {
+						buffer.append("maa.name ");
+						if (entry.getValue() == SortOrder.DESCENDING) {
+							buffer.append("desc ");
+						}
+					}
+					index++;
+				}
+			}
+
+			search.setQuery(buffer.toString());
+		}
+		return search;
+	}
+}

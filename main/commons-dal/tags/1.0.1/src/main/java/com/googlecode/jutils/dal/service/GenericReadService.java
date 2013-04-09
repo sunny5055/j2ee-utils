5b4d5e472d4694cb94fded2902b9f626b23a0ca7
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-04-09 09:02:52 +0000

    [maven-release-plugin]  copy for tag 1.0.1

---
+++ main/commons-dal/tags/1.0.1/src/main/java/com/googlecode/jutils/dal/service/GenericReadService.java
@@ -0,0 +1,78 @@
+package com.googlecode.jutils.dal.service;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+import com.googlecode.jutils.dal.SearchCriteria;
+import com.googlecode.jutils.dal.dto.Dto;
+
+/**
+ * The Interface GenericReadService.
+ * 
+ * @param <PK>
+ *            the generic type
+ * @param <E>
+ *            the element type
+ */
+public interface GenericReadService<PK extends Serializable, E extends Dto<PK>> {
+
+	/**
+	 * Gets the entity.
+	 * 
+	 * @param pk
+	 *            the primary key
+	 * @return the entity
+	 */
+	E get(PK pk);
+
+	/**
+	 * Gets the entities.
+	 * 
+	 * @param pks
+	 *            the primary keys
+	 * @return the objects
+	 */
+	List<E> getObjects(Collection<PK> pks);
+
+	/**
+	 * Count.
+	 * 
+	 * @return the integer
+	 */
+	Integer count();
+
+	/**
+	 * Finds all entities.
+	 * 
+	 * @return the list
+	 */
+	List<E> findAll();
+
+	/**
+	 * Count.
+	 * 
+	 * @param searchCriteria
+	 *            the search criteria
+	 * @return the integer
+	 */
+	Integer count(SearchCriteria searchCriteria);
+
+	/**
+	 * Finds all entities.
+	 * 
+	 * @param searchCriteria
+	 *            the search criteria
+	 * @return the list
+	 */
+	List<E> findAll(SearchCriteria searchCriteria);
+
+	/**
+	 * Exist primary key.
+	 * 
+	 * @param pk
+	 *            the primary key
+	 * @return true, if successful
+	 */
+	boolean existPk(PK pk);
+}

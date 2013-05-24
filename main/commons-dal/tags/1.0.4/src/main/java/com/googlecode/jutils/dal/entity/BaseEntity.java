9baa8f9cee66e7082d89077ab69cccbf8f0b8260
jeremy.schaal@gmail.com <jeremy.schaal@gmail.com@bf5b354c-f891-77f7-e673-8c491c455983>
2013-05-24 15:23:12 +0000

    [maven-release-plugin]  copy for tag 1.0.4

---
+++ main/commons-dal/tags/1.0.4/src/main/java/com/googlecode/jutils/dal/entity/BaseEntity.java
@@ -0,0 +1,34 @@
+package com.googlecode.jutils.dal.entity;
+
+import java.io.Serializable;
+
+/**
+ * The Interface BaseEntity.
+ * 
+ * @param <PK>
+ *            the generic type
+ */
+public interface BaseEntity<PK extends Serializable> extends Serializable {
+
+	/**
+	 * Getter : return the primaryKey
+	 * 
+	 * @return the primaryKey
+	 */
+	PK getPrimaryKey();
+
+	/**
+	 * Setter : affect the primaryKey
+	 * 
+	 * @param primaryKey
+	 *            the primaryKey
+	 */
+	void setPrimaryKey(PK primaryKey);
+
+	/**
+	 * Gets the string primary key.
+	 * 
+	 * @return the string primary key
+	 */
+	String getStringPrimaryKey();
+}
